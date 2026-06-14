7940f7621213c59aeea2c0f817a08697e99e7fa8
tommic81 <tom.michnik@gmail.com>
2026-06-14 23:07:40 +0200

    Spring Web Client : Beer Client, docs

---
+++ reactive-beer-client/src/main/java/guru/springframework/reactivebeerclient/client/BeerClient.java
@@ -0,0 +1,23 @@
+package guru.springframework.reactivebeerclient.client;
+
+import guru.springframework.reactivebeerclient.model.BeerDto;
+import guru.springframework.reactivebeerclient.model.BeerPagedList;
+import org.springframework.http.ResponseEntity;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+public interface BeerClient {
+    Mono<BeerDto> getBeerById(UUID id, Boolean showInventoryOnHand);
+
+    Mono<BeerPagedList> listBeers(Integer pageNumber, Integer pageSize, String beerName,
+                                  String beerStyle, Boolean showInventoryOnhand);
+
+    Mono<ResponseEntity> createBeer(BeerDto beerDto);
+
+    Mono<ResponseEntity> updateBeer(BeerDto beerDto);
+
+    Mono<ResponseEntity> deleteBeerById(UUID id);
+
+    Mono<BeerDto> getBeerByUPC(String upc);
+}
